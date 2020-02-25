7eebdd496baaf7421d6ac47077ba6f4a5196ec5f
Phoebe0126 <1542756558@qq.com>
2020-02-25 17:44:27 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 // 移动端1像素边框问题
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 // 300毫秒延迟问题
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
