b9e95673d27d647bd8c81f8f68973c5f965f89b9
Phoebe0126 <1542756558@qq.com>
2020-02-25 16:20:46 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+// 移动端1像素边框问题
+import 'styles/border.css'
+import 'styles/iconfont.css'
 Vue.config.productionTip = false
-
+// 300毫秒延迟问题
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
