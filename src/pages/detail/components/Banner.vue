7970d721fff28221a51d82f0e4cba7ff1a0bc5bd
Phoebe0126 <1542756558@qq.com>
2020-03-02 16:08:24 +0800

    add animation

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,30 +1,33 @@
 <template>
-<div>
+  <div>
     <div class="banner" @click="handleBannerClick">
-        <img class="banner-img"
-        :src="bannerImg">
-        <div class="banner-info">
-            <div class="banner-title">{{sightName}}</div>
-            <div class="banner-number">
-                <span class="iconfont banner-icon">&#xe695;</span>
-                39</div>
+      <img class="banner-img" :src="bannerImg" />
+      <div class="banner-info">
+        <div class="banner-title">{{ sightName }}</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe695;</span>
+          39
         </div>
+      </div>
     </div>
-    <common-gallery
-    :imgs="gallleryImgs"
-    v-show="showGallery"
-    @close="closeGallery"
-    ></common-gallery>
-</div>
-
+    <fade-animation>
+      <common-gallery
+        :imgs="gallleryImgs"
+        v-show="showGallery"
+        @close="closeGallery"
+      ></common-gallery>
+    </fade-animation>
+  </div>
 </template>
 
 <script>
-import CommonGallery from 'common/Gallery'
+import CommonGallery from 'common/gallery/Gallery'
+import FadeAnimation from 'common/fade/fade'
 export default {
   name: 'DetailBanner',
   components: {
-    CommonGallery
+    CommonGallery,
+    FadeAnimation
   },
   props: {
     sightName: String,
@@ -48,34 +51,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-   .banner
-     position: relative
-     overflow: hidden
-     height: 0
-     padding-bottom: 55%
-     .banner-img
-       width: 100%
-     .banner-info
-       position: absolute
-       left: 0
-       right: 0
-       bottom: 0
-       line-height: .6rem
-       display: flex
-       color: #fff
-       background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
-       .banner-title
-          flex: 1
-          font-size: .32rem
-          padding: 0 .2rem
-       .banner-number
-          line-height: .32rem
-          height: .32rem
-          border-radius: .2rem
-          background: rgba(0, 0, 0, .8)
-          font-size: .24rem
-          margin-top: .14rem
-          padding: 0 .4rem
-        .banner-icon
-          font-size: .24rem
+.banner
+  position: relative
+  overflow: hidden
+  height: 0
+  padding-bottom: 55%
+  .banner-img
+    width: 100%
+  .banner-info
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    line-height: .6rem
+    display: flex
+    color: #fff
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+    .banner-title
+       flex: 1
+       font-size: .32rem
+       padding: 0 .2rem
+    .banner-number
+       line-height: .32rem
+       height: .32rem
+       border-radius: .2rem
+       background: rgba(0, 0, 0, .8)
+       font-size: .24rem
+       margin-top: .14rem
+       padding: 0 .4rem
+     .banner-icon
+       font-size: .24rem
 </style>
